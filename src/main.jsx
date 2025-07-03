2b977991f08f93ff4af95ff6085a531832affd53
sgzs <sgzs6721@gmail.com>
2025-07-04 01:30:31 +0800

    Add dayjs localization for Chinese in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
+dayjs.locale('zh-cn')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
