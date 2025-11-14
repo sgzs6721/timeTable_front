087be40a2d4ac6cf5be6d70712bcad78698678a8
sgzs <sgzs6721@gmail.com>
2025-11-14 23:49:12 +0800

    [fix][ui][Update company name in Footer component from 雷网科技（北京）有限公司 to 飓风科技（北京）有限公司 for accurate branding]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         © 2023-{new Date().getFullYear()} 飓风乒乓培训中关村校区
       </div>
       <div style={{ textAlign: 'center', color: '#999', fontSize: '12px', marginTop: '2px' }}>
-        雷网科技（北京）有限公司
+        飓风科技（北京）有限公司
       </div>
     </div>
   );
